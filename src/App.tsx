691ba00e72fb7d09f1c54fbbfb5051abf7550ae6
Felipe Moura <felipe.moura@interprocess.com.br>
2021-09-14 21:22:57 -0300

    ajustes de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header/Index';
 import { GlobalStyles } from './styles/global'
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 
 export function App() {
+  const [isOpenNewTransactonModal, setIsNewTransactioconModal] = useState(false)
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactioconModal(true)
+  }
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactioconModal(false)
+  }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransacitonModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal isOpen={isOpenNewTransactonModal} handleCloseModal={handleCloseNewTransactionModal} />
       <GlobalStyles />
     </>
   );
